8a012a8bd468b2d3ac03d760355d425c51ee22f7
Cyrus Chan <cyruschan4561@gmail.com>
2021-05-05 20:53:05 -0700

    Unit tests for routes are now comprehensive (covers all possible routes)

--- reminderAppStarterFile/test_auth_routes.js
+++ reminderAppStarterFile/test_auth_routes.js
@@ -18,9 +18,163 @@ function loginUser() {
     };
 }
 
-describe("Tests routes that requires authentication", function () {
-  it("logs a user in", loginUser());
-  it("accesses a protected page that requires user to be logged in", function (done) {
+function testIncorrectCredentials() {
+    return function (done) {
+      server
+        .post("/login")
+        .send({ email: "123@gmail.com", password: "wrongpassword" })  // Incorrect credentials
+        .expect(302)
+        .expect("Location", "/login")
+        .end(onResponse);
+  
+      function onResponse(err, res) {
+        if (err) return done(err);
+        return done();
+      }
+    };
+}
+
+describe("Test routes without being authenticated", function () {
+    it("access / (expect 200)", function (done) {
+        server
+          .get("/")
+          .expect(200)
+          .end(function (err, res) {
+            if (err) return done(err);
+            //console.log(res.text);
+            done();
+          });
+      });
+  
+      it("access /register (expect 200)", function (done) {
+        server
+          .get("/register")
+          .expect(200)
+          .end(function (err, res) {
+            if (err) return done(err);
+            //console.log(res.text);
+            done();
+          });
+      });
+
+      it("access /login (expect 200)", function (done) {
+        server
+          .get("/login")
+          .expect(200)
+          .end(function (err, res) {
+            if (err) return done(err);
+            //console.log(res.text);
+            done();
+          });
+      });
+
+      it("access /routedoesnotexist (expect 404)", function (done) {
+        server
+          .get("/routedoesnotexist")
+          .expect(404)
+          .end(function (err, res) {
+            if (err) return done(err);
+            //console.log(res.text);
+            done();
+          });
+      });
+
+      it("access /reminders (expect 302 as we are not authenticated)", function (done) {
+        server
+          .get("/reminders")
+          .expect(302)
+          .end(function (err, res) {
+            if (err) return done(err);
+            //console.log(res.text);
+            done();
+          });
+      });
+
+      it("access /reminder/new (expect 302 as we are not authenticated)", function (done) {
+        server
+          .get("/reminder/new")
+          .expect(302)
+          .end(function (err, res) {
+            if (err) return done(err);
+            //console.log(res.text);
+            done();
+          });
+      });
+
+      it("access /reminder/1 (expect 302 as we are not authenticated)", function (done) {
+        server
+          .get("/reminder/1")
+          .expect(302)
+          .end(function (err, res) {
+            if (err) return done(err);
+            //console.log(res.text);
+            done();
+          });
+      });
+
+      it("access /reminder/2 (expect 302 as we are not authenticated)", function (done) {
+        server
+          .get("/reminder/2")
+          .expect(302)
+          .end(function (err, res) {
+            if (err) return done(err);
+            //console.log(res.text);
+            done();
+          });
+      });
+  
+});
+
+describe("Test routes that require authentication while authenticated", function () {
+  it("test ability to log a user in", loginUser());
+  it("test ability to login using bad credentials (expect 302, rerouted to /login and not routed to /reminders)", testIncorrectCredentials());
+
+
+  it("access / (expect 200)", function (done) {
+    server
+      .get("/")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        //console.log(res.text);
+        done();
+      });
+  });
+
+  it("access /register (expect 302 as we are authenticated)", function (done) {
+    server
+      .get("/register")
+      .expect(302)
+      .end(function (err, res) {
+        if (err) return done(err);
+        //console.log(res.text);
+        done();
+      });
+  });
+
+  it("access /login (expect 302 as we are authenticated)", function (done) {
+    server
+      .get("/login")
+      .expect(302)
+      .end(function (err, res) {
+        if (err) return done(err);
+        //console.log(res.text);
+        done();
+      });
+  });
+
+  it("access /routedoesnotexist (expect 404)", function (done) {
+    server
+      .get("/routedoesnotexist")
+      .expect(404)
+      .end(function (err, res) {
+        if (err) return done(err);
+        //console.log(res.text);
+        done();
+      });
+  });
+
+  it("access /reminders (expect 200 as we are authenticated)", function (done) {
     server
       .get("/reminders")
       .expect(200)
@@ -31,15 +185,38 @@ describe("Tests routes that requires authentication", function () {
       });
   });
 
-  it("tests the response code of a route that does not exist", function (done) {
+  it("access /reminder/new (expect 200 as we are authenticated)", function (done) {
     server
-      .get("/routedoesnotexist")
-      .expect(404)
+      .get("/reminder/new")
+      .expect(200)
       .end(function (err, res) {
         if (err) return done(err);
         //console.log(res.text);
         done();
       });
   });
+
+  it("access /reminder/1 (expect 200 as we are authenticated)", function (done) {
+    server
+      .get("/reminder/1")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        //console.log(res.text);
+        done();
+      });
+  });
+
+  it("access /reminder/2 (expect 200 as we are authenticated)", function (done) {
+    server
+      .get("/reminder/2")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        //console.log(res.text);
+        done();
+      });
+  });
+
 
 });
