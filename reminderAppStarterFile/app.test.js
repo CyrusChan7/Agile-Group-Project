646cddb9b38fd0914055cfada21d140c41f1ca5f
Cyrus Chan <cyruschan4561@gmail.com>
2021-05-08 17:56:00 -0700

    Setup template for unit testing mock event creation

---
+++ reminderAppStarterFile/app.test.js
@@ -0,0 +1,40 @@
+const app = require("./index.js");
+const request = require("supertest");
+const server = request.agent(app);
+const database = require("./database.js").Database;
+
+
+
+function loginUser() {
+  return function (done) {
+    server
+      .post("/login")
+      .send({ email: "123@gmail.com", password: "123" }) // Cindy user, expect to be 302 as the credentials are correct
+      .expect(302)
+      .expect("Location", "/reminders")
+      .then(() => done());
+  };
+}
+
+describe("Mock event creation", function () {
+  it("test ability to log a user in", loginUser());
+  it("/reminder/", function (done) {
+    server
+      .post("/reminder/")
+      .send({
+        title: "feed the dog",
+        description: "feed her before tomorrow at noon",
+        completed: false,
+        image_url: "/Reminder.svg",
+        subtasks: [],
+        date: "05/20/2021",
+        tags: "Delicious food",
+      })
+      .then((res) => database)
+      .then((db) => {
+        console.log(db[0].reminders);
+        expect(db[0].reminders.length).toEqual(3);
+      })
+      .then(() => done());
+  });
+});
--- reminderAppStarterFile/controller/reminder_controller.js
+++ reminderAppStarterFile/controller/reminder_controller.js
@@ -1,171 +1,173 @@
 let database = require("../database").Database;
 const fetch = require("node-fetch");
-const { calendarData } = require("../views/reminder/scripts/calendar")
-
+const { calendarData } = require("../views/reminder/scripts/calendar");
 
 let remindersController = {
-    // List out all the reminder
-    list: (req, res) => {
-        res.render("reminder/index", {
-            user: req.user,
-            reminders: req.user.reminders,
-            database: database,
-            friendIDs: req.user.friends.friendID,
-            calendarData,
+  // List out all the reminder
+  list: (req, res) => {
+    res.render("reminder/index", {
+      user: req.user,
+      reminders: req.user.reminders,
+      database: database,
+      friendIDs: req.user.friends.friendID,
+      calendarData,
+    });
+  },
 
-        });
-    },
+  new: (req, res) => {
+    res.render("reminder/create");
+    // console.log(req.user);
+  },
 
-    new: (req, res) => {
-        res.render("reminder/create");
-        // console.log(req.user);
-    },
+  listOne: (req, res) => {
+    // List only one reminder
+    const reminderToFind = req.params.id;
+    const searchResult = req.user.reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    });
+    if (searchResult != undefined) {
+      res.render("reminder/single-reminder", { reminderItem: searchResult });
+    } else {
+      res.render("reminder/index", {
+        user: req.user,
+        reminders: req.user.reminders,
+        database: database,
+        friendIDs: req.user.friends.friendID,
+      });
+    }
+  },
 
-    listOne: (req, res) => {
-        // List only one reminder
-        const reminderToFind = req.params.id;
-        const searchResult = req.user.reminders.find(function(reminder) {
-            return reminder.id == reminderToFind;
-        });
-        if (searchResult != undefined) {
-            res.render("reminder/single-reminder", { reminderItem: searchResult });
-        } else {
-            res.render("reminder/index", {
-                user: req.user,
-                reminders: req.user.reminders,
-                database: database,
-                friendIDs: req.user.friends.friendID 
-            });
-        }
-    },
+  searchBarResults: (req, res) => {
+    // Search for reminder based on its title name
+    let searchResultsDatabase = [];
+    const userSearchTerm = req.query.search;
 
-    searchBarResults: (req, res) => {
-        // Search for reminder based on its title name
-        let searchResultsDatabase = []
-        const userSearchTerm = req.query.search;
+    //console.log(`DEBUG: userSearchTerm is: ${userSearchTerm}`);
 
-        //console.log(`DEBUG: userSearchTerm is: ${userSearchTerm}`);
+    for (let i = 0; i < req.user.reminders.length; i++) {
+      // if substring found
+      if (req.user.reminders[i].title.includes(userSearchTerm)) {
+        searchResultsDatabase.push(req.user.reminders[i]);
+      }
+    }
+    res.render("reminder/index", {
+      user: req.user,
+      reminders: searchResultsDatabase,
+      database: database,
+      friendIDs: req.user.friends.friendID,
+      calendarData,
+    });
+  },
 
-        for (let i = 0; i < req.user.reminders.length; i++) {
-            // if substring found
-            if (req.user.reminders[i].title.includes(userSearchTerm)) {
-                searchResultsDatabase.push(req.user.reminders[i]);
-            }
-        }
-        res.render("reminder/index", {
-            user: req.user,
-            reminders: searchResultsDatabase,
-            database: database,
-            friendIDs: req.user.friends.friendID,
-            calendarData,
-        });
-    },
+  // Create a new reminder
+  create: async (req, res) => {
+    // const tempSubtasks = req.body.subtasks.split(",");
+    const tempSubtasks = [];
+    let idNum = Number(1);
+    if (req.user.reminders.length != 0) {
+      idNum = Number(req.user.reminders[req.user.reminders.length - 1].id) + 1;
+    }
+    const reminder = {
+      id: idNum,
+      title: req.body.title,
+      description: req.body.description,
+      completed: false,
+      image_url: "",
+      tags: req.body.tags,
+      subtasks: tempSubtasks,
+      date: req.body.date.replace("T", " "),
+    };
+    //console.log(`DEBUG create tempSubtasks is: ${tempSubtasks}`)
+    //console.log(typeof tempSubtasks);
 
-    // Create a new reminder
-    create: async(req, res) => {
-        const tempSubtasks = req.body.subtasks.split(",");
-        let idNum = Number(1)
-        if (req.user.reminders.length != 0){
-            idNum = Number(req.user.reminders[req.user.reminders.length -1].id) +1;
-        }
-        const reminder = {
-            id: idNum,
-            title: req.body.title,
-            description: req.body.description,
-            completed: false,
-            image_url: "",
-            tags: req.body.tags,
-            subtasks: tempSubtasks,
-            date: req.body.date.replace("T", " ")
-        };
-        //console.log(`DEBUG create tempSubtasks is: ${tempSubtasks}`)
-        //console.log(typeof tempSubtasks);
+    // Use Unsplash API to fetch images for reminders
+    const client_id = process.env.Unsplash_CLIENT_ID;
+    const photos = await fetch(
+      `https://api.unsplash.com/photos/random?query=${reminder.title}&client_id=${client_id}`
+    );
 
-        // Use Unsplash API to fetch images for reminders
-        const client_id = process.env.Unsplash_CLIENT_ID
-        const photos = await fetch(`https://api.unsplash.com/photos/random?query=${reminder.title}&client_id=${client_id}`)
+    const parsedPhotos = await photos.json();
 
-        const parsedPhotos = await photos.json();
+    if ("errors" in parsedPhotos) {
+      console.log("ERROR: Cannot find image!");
+      reminder.image_url = "/Reminder.svg";
+      req.user.reminders.push(reminder);
+    } else {
+      // Save the URL into reminder object
+      reminder.image_url = parsedPhotos.urls.regular;
 
-        if ("errors" in parsedPhotos) {
-            console.log("ERROR: Cannot find image!")
-            reminder.image_url = "/Reminder.svg"
-            req.user.reminders.push(reminder);
-        } else {
+      req.user.reminders.push(reminder);
+    }
 
-            // Save the URL into reminder object
-            reminder.image_url = parsedPhotos.urls.regular
+    res.redirect("/reminders");
+  },
 
-            req.user.reminders.push(reminder);
-        }
+  // Edit a specific reminder
+  edit: (req, res) => {
+    const reminderToFind = req.params.id;
+    const searchResult = req.user.reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    });
+    res.render("reminder/edit", { reminderItem: searchResult });
+  },
 
-        res.redirect("/reminders");
-    },
+  // Update a specific reminder
+  update: (req, res) => {
+    // Loop through all reminders and update the correct one (id)
+    const tempSubtasks = req.body.subtasks.split(",");
+    req.user.reminders.forEach((reminder) => {
+      if (String(reminder.id) === req.params.id) {
+        reminder.title = req.body.title;
+        reminder.description = req.body.description;
+        reminder.completed = req.body.completed == "true";
+        reminder.tags = req.body.tags;
+        reminder.subtasks = tempSubtasks;
+        reminder.date = req.body.date.replace("T", " ");
+      }
+      // console.log(`DEBUG update tempSubtasks is: ${tempSubtasks}`)
+      // console.log(typeof tempSubtasks);
+    });
+    res.redirect("/reminder/" + req.params.id);
+  },
 
-    // Edit a specific reminder
-    edit: (req, res) => {
-        const reminderToFind = req.params.id; 
-        const searchResult = req.user.reminders.find(function(reminder) {
-            return reminder.id == reminderToFind;
-        });
-        res.render("reminder/edit", { reminderItem: searchResult });
-    },
+  // Delete reminder based on reminder's id
+  delete: (req, res) => {
+    const reminderToFind = req.params.id;
 
-    // Update a specific reminder
-    update: (req, res) => {
-        // Loop through all reminders and update the correct one (id)
-        const tempSubtasks = req.body.subtasks.split(",");
-        req.user.reminders.forEach((reminder) => {
-            if (String(reminder.id) === req.params.id) {
-                reminder.title = req.body.title;
-                reminder.description = req.body.description;
-                reminder.completed = (req.body.completed == 'true');
-                reminder.tags = req.body.tags;
-                reminder.subtasks = tempSubtasks;
-                reminder.date = req.body.date.replace("T", " ");
-            }
-            // console.log(`DEBUG update tempSubtasks is: ${tempSubtasks}`)
-            // console.log(typeof tempSubtasks);
-        });
-        res.redirect("/reminder/" + req.params.id);
-    },
+    req.user.reminders.forEach((reminder) => {
+      reminder.id = String(reminder.id);
+    });
 
-    // Delete reminder based on reminder's id
-    delete: (req, res) => {
-        const reminderToFind = req.params.id;
+    // Not found by default
+    let index = -1;
+    for (let i = 0; i < req.user.reminders.length; i++) {
+      if (req.user.reminders[i].id === reminderToFind) {
+        //console.log(i);
+        index = i;
+        break;
+      }
+    }
 
-        req.user.reminders.forEach((reminder) => {
-            reminder.id = String(reminder.id);
-        });
+    const result = req.user.reminders.filter(({ id }) =>
+      id.includes(reminderToFind)
+    );
+    //console.log(result);
 
-        // Not found by default
-        let index = -1
-        for (let i = 0; i < req.user.reminders.length; i++) {
-            if (req.user.reminders[i].id === reminderToFind) {
-                //console.log(i);
-                index = i;
-                break;
-            }
-        }
+    // Remove array element based on index position
+    if (index >= 0) {
+      req.user.reminders.splice(index, 1);
+    }
 
-        const result = req.user.reminders.filter(({ id }) => id.includes(reminderToFind))
-            //console.log(result);
+    res.redirect("/reminders");
+  },
 
-        // Remove array element based on index position    
-        if (index >= 0) {
-            req.user.reminders.splice(index, 1);
-        }
-
-        res.redirect("/reminders");
-    },
-
-    // Showing friend's reminder on the main page
-    listFriends: (req, res) => {
-        res.render("reminder/friends", {
-            user: req.user,
-            database: database,
-        });
-    },
+  // Showing friend's reminder on the main page
+  listFriends: (req, res) => {
+    res.render("reminder/friends", {
+      user: req.user,
+      database: database,
+    });
+  },
 };
 
 module.exports = remindersController;
