e39b25f0f4154818d4d760f0dd892b7755cd4ccd
Cyrus Chan <cyruschan4561@gmail.com>
2021-05-12 17:05:15 -0700

    Unit tests now test specific reminder attributes

--- reminderAppStarterFile/app.test.js
+++ reminderAppStarterFile/app.test.js
@@ -22,7 +22,7 @@ describe("Mock event creation", function () {
     server
       .post("/reminder/")
       .send({
-        title: "feed the dog",
+        title: "test feed the dog",
         description: "feed her before tomorrow",
         completed: false,
         image_url: "/Reminder.svg",
@@ -32,7 +32,41 @@ describe("Mock event creation", function () {
       })
       .then((res) => database)
       .then((db) => {
-        //console.log(db[0].reminders);
+        expect(db[0].reminders[reminderLen].title).toEqual("test feed the dog")
+        expect(db[0].reminders[reminderLen].description).toEqual("feed her before tomorrow")
+        expect(db[0].reminders[reminderLen].completed).toEqual(false)
+        expect(db[0].reminders[reminderLen].subtasks).toEqual([""])
+        expect(db[0].reminders[reminderLen].date).toEqual("05/20/2021")
+        expect(db[0].reminders[reminderLen].tags).toEqual("Delicious food")
+        expect(db[0].reminders.length).toEqual(reminderLen + 1);
+      })
+      .then(() => done());
+  });
+});
+
+
+describe("Mock event creation 2", function () {
+  it("mock event creation 2", function (done) {
+    const reminderLen = database[0].reminders.length
+    server
+      .post("/reminder/")
+      .send({
+        title: "test feed the cat",
+        description: "feed her before sunset",
+        completed: false,
+        image_url: "/Reminder.svg",
+        subtasks: "it, needs, happen, quite, soon",
+        date: "05/21/2021",
+        tags: "Delicious, food, at, my, fingertips",
+      })
+      .then((res) => database)
+      .then((db) => {
+        expect(db[0].reminders[reminderLen].title).toEqual("test feed the cat")
+        expect(db[0].reminders[reminderLen].description).toEqual("feed her before sunset")
+        expect(db[0].reminders[reminderLen].completed).toEqual(false)
+        expect(db[0].reminders[reminderLen].subtasks).toEqual(["it", " needs", " happen", " quite", " soon"])
+        expect(db[0].reminders[reminderLen].date).toEqual("05/21/2021")
+        expect(db[0].reminders[reminderLen].tags).toEqual("Delicious, food, at, my, fingertips")
         expect(db[0].reminders.length).toEqual(reminderLen + 1);
       })
       .then(() => done());
